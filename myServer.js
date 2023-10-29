357ae467f68d279006f933867d37cc30a1b23e20
Alukard <kirill1974111@gmail.com>
2023-10-29 20:41:11 +0100

    #8 creating server and routing

--- myServer.js
+++ myServer.js
@@ -1,5 +1,6 @@
 // Вначале импортируем библотеку http или https
 const http = require('http')
+const fs = require('fs')
 
 // потом созлаем сервер
 // куда передаем йункцию Call-Back, котоая принимает request и response
@@ -9,7 +10,23 @@ const http = require('http')
 // в полследнюю очередь сам контент, который будет на сайте 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': "text/html; charset=utf-8"})
-    response.end("Hello <b>Node JS</b>")
+    // response.end(String) - так лечше не писать
+    // response.end("Hello <b>Node JS</b>")
+
+    if(request.url === "/"){
+        // Сначала мы считывыем все данные с index.html
+        // тут createStreamRead ипользует ПОТОКОВЫЙ подход
+        // то есть он не дожидается пока весь файл будет считан, а отправляет частями
+        // .pipe(response) - когда видид, что пришла новая порция, загружает ее
+        const stream = fs.createReadStream('./templates/index.html')
+        stream.pipe(response)
+    }else if(request.url === "/about"){
+        const stream = fs.createReadStream('./templates/about.html')
+        stream.pipe(response)
+    }else{
+        const stream = fs.createReadStream('./templates/error.html')
+        stream.pipe(response)
+    }
 })
 
 
