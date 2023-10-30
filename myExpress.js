df4193b152d79d260d81838a47bf777b550cee14
Alukard <kirill1974111@gmail.com>
2023-10-30 17:12:12 +0100

    #12 data from user

--- myExpress.js
+++ myExpress.js
@@ -1,9 +1,11 @@
 // npm i express
 // npm i ejs
+// npm i body-parser
 const express = require('express')//импорт Express JS
 
 const app = express()//создаение сервера
 app.set('view engine', "ejs")//добавление ШАБЛОНИЗАТОРА на сервер
+app.use(express.urlencoded({ extended: false }))// добавление нормально парсинга для форм
 app.use(express.static('public'))//установление СТАТИЧЕСКОЙ папку, для подключения стилей
 
 app.get('/', (request, response) => {//получение главной страницы
@@ -26,6 +28,14 @@ app.get('/about', (request, response) => {
 app.get("/user/:userName/:id", (request, response) => {
     const hobbies = ['football', 'basketball', "skiing"]
     response.render('user', {userName: request.params.userName, id: request.params.id, hobbies})
+})
+
+app.post('/search-user', (request, response) => {
+    const userName = request.body.userName
+    if(userName.length === 0)
+        return response.redirect('/')
+    else
+        return response.redirect(`/user/${userName}/1`)
 })
 
 const PORT = 3000
